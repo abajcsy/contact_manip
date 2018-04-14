a64e963accc5cb339d5c7378922080f07cc9f581
abajcsy <andrea.victoria.b@gmail.com>
2018-04-13 17:19:17 -0700

    twolinkarmwithdoor almost complete...need to fix phi function. added visualization functionality

--- matlab/test_TwoLinkArm.m
+++ matlab/test_TwoLinkArm.m
@@ -1,9 +1,9 @@
 % Test the dynamics
 arm = TwoLinkArm(2, 1, 1, 1, 1, 1);
 
-q_init = [pi / 2; pi / 2];
-dq_init = [0.01; 0.01];
+q_init = [2*pi/3; pi/4];
+dq_init = [-0.1; 0];
 dt = 0.01;
-T = 5000;
+T = 20000;
 
-arm.simulate(q_init, dq_init, dt, T);
+arm.simulate_no_contact(q_init, dq_init, dt, T);
